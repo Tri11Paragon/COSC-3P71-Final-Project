1b82d844571fe7c03cbfad434b52bd1eb609ec16
Brett <brettmaster1@gmail.com>
2022-12-16 21:59:11 -0500

    Added inital code

---
+++ commit.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git add *
+git commit
+git push -u origin main
+git push -u tpgc main
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,7 @@
+package chess;
+
+public abstract class ChessPiece {
+
+    public abstract void move();
+
+}
